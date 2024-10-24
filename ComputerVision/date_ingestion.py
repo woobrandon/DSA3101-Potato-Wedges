9a83f9cac6d0f4a026c3bda03e927bd0c00e364c
happlesful <Swislar1108@gmail.com>
2024-10-24 16:40:38 +0800

    Added comments to functions

--- ComputerVision/date_ingestion.py
+++ ComputerVision/date_ingestion.py
@@ -13,21 +13,28 @@ import time
 import os
 import requests
 
+# initialise the model using the weights obtained from training
 model = ResNet50(weights='resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
                  include_top=False, pooling='avg')
 
 amazon_df = pd.read_csv("amazon.csv")
 
-# function extracts the features from each image
+
 def extract_features(img_path):
+    """
+    Extract the features from each image
+    """
     img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = preprocess_input(x)
     features = model.predict(x[None, ...])
     return features
 
-# creating a table with the columns id, filename and features
+
 def create_table():
+    """
+    Create a table with the columns id, filename and features
+    """
     # Connect to the SQLite database (or create it if it doesn't exist)
     conn = sqlite3.connect('feature_database.db')
     cursor = conn.cursor()
@@ -42,48 +49,57 @@ def create_table():
     ''')
     conn.commit()
     conn.close()
+    print("Table initialised!")
+    return
 
 
-create_table()
+def scrape_amzn_images():
+    """
+    Downloads the images from the amazon csv file either through the image url or scraping the amazon website
+    """
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+    }
+    images = amazon_df[["img_link", "product_link"]]
+    n = len(images)
+    temp = 0
+    service = Service('./chromedriver')
+    driver = webdriver.Chrome(service=service)
 
-# downloadng the images from the amazon csv file either through the image url or scraping the amazon website
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
-}
-images = amazon_df[["img_link", "product_link"]]
-n = len(images)
-temp = 0
-service = Service('./chromedriver')
-driver = webdriver.Chrome(service=service)
+    for i in range(1, n+1):
+        response = requests.get(images.iloc[i-1]["img_link"])
+        if response.status_code == 200:
+            with open("amazon_images/image_" + str(temp) + ".jpg", "wb") as file:
+                file.write(response.content)
+            temp += 1
+        else:
+            response = requests.get(images.iloc[i-1]["product_link"])
+            product_link = images.iloc[i-1]["product_link"]
+            driver.get(product_link)
+            time.sleep(3)
 
-for i in range(1, n+1):
-    response = requests.get(images.iloc[i-1]["img_link"])
-    if response.status_code == 200:
-        with open("amazon_images/image_" + str(temp) + ".jpg", "wb") as file:
-            file.write(response.content)
-        temp += 1
-    else:
-        response = requests.get(images.iloc[i-1]["product_link"])
-        product_link = images.iloc[i-1]["product_link"]
-        driver.get(product_link)
-        time.sleep(3)
+            try:
+                image_tag = driver.find_element(By.ID, 'landingImage')
+                soup = BeautifulSoup(response.content, 'html.parser')
+                image_url = image_tag.get_attribute('src')
 
-        try:
-            image_tag = driver.find_element(By.ID, 'landingImage')
-            soup = BeautifulSoup(response.content, 'html.parser')
-            image_url = image_tag.get_attribute('src')
+                if image_url:
 
-            if image_url:
+                    img_response = requests.get(image_url)
+                    img_response.raise_for_status()
+                    with open("amazon_images/image_" + str(temp) + ".jpg", "wb") as file:
+                        file.write(img_response.content)
+                    temp += 1
+            except Exception as e:
+                print("file number" + str(i) + "did not print")
+    print("Scraping complete!")
+    return
 
-                img_response = requests.get(image_url)
-                img_response.raise_for_status()
-                with open("amazon_images/image_" + str(temp) + ".jpg", "wb") as file:
-                    file.write(img_response.content)
-                temp += 1
-        except Exception as e:
-            print("file number" + str(i) + "did not print")
 
 def insert_features(id, filename, features):
+    """
+    Inserts features into database after extracting it with the extract function
+    """
     conn = sqlite3.connect('feature_database.db')
     cursor = conn.cursor()
 
@@ -96,16 +112,23 @@ def insert_features(id, filename, features):
 
     conn.commit()
     conn.close()
+    return
 
+
+create_table()
 
 folder_path = 'amazon_images/'
-temp = 0
 
-for img in os.listdir(folder_path):
-    try:
-        img_path = os.path.join(folder_path, img)
-        img_features = extract_features(img_path)
-        insert_features(temp, img, img_features)
-        temp += 1
-    except:
-        print(str(img) + " was unable to be inserted")
+if not os.path.exists(folder_path):
+    scrape_amzn_images()
+
+db_id = 0
+
+# for img in os.listdir(folder_path):
+#     try:
+#         img_path = os.path.join(folder_path, img)
+#         img_features = extract_features(img_path)
+#         insert_features(db_id, img, img_features)
+#         db_id += 1
+#     except:
+#         print(str(img) + " was unable to be inserted")
