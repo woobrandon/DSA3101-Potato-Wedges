88c4d83e5a399ac225ea2ff34d96a8aa12e31f7e
happlesful <Swislar1108@gmail.com>
2024-10-31 11:19:24 +0800

    Updated imports

--- ComputerVision/frontend/src/App.tsx
+++ ComputerVision/frontend/src/App.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home"
-import ProductFinder from './pages/ProductFinder';
-import ProductCategorization from './pages/ProductCategorization';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, ProductFinder, ProductCategorization } from "./pages";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path = "/" element = {<Home />}></Route>
-        <Route path = "/ProductFinder" element = {<ProductFinder />} />
-        <Route path = "/ProductCategorization" element = {<ProductCategorization />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/ProductFinder" element={<ProductFinder />} />
+        <Route
+          path="/ProductCategorization"
+          element={<ProductCategorization />}
+        ></Route>
       </Routes>
     </Router>
   );
