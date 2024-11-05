38e06248a02658fe242ec452c5482db76b335ee1
brandon <brandonwookeanjin@gmail.com>
2024-11-05 23:04:32 +0800

    small css fixes

--- ComputerVision/frontend/src/components/Header.tsx
+++ ComputerVision/frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from "./Header.module.css";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png"
 
 const Header = () => {
