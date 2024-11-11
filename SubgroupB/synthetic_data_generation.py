9ff937f015c9886603433c300b48edb28e607c1e
DSA-Noob <captainshiba@gmail.com>
2024-11-12 00:04:52 +0800

    Removed price model and synthetic_data_generation inside Subgroup B

--- SubgroupB/synthetic_data_generation.py
+++
@@ -1,8 +0,0 @@
-from scipy.stats import gaussian_kde
-
-sample = pd.read_csv('sample.csv') # load the sample data that you will use to generate new data
-df = pd.read_csv('df.csv') # load the dataset that you want to generate new columns for 
-n_train = df.shape[0]
-
-kde = gaussian_kde(sample[["col"]])
-df["col"] = kde.resample(n_train).flatten().astype(int)
